1d6e66e5fcc6d52344c6e27717a0eea6b20ce096
Sabasiva2916 <sabasiva2916@gmail.com>
2024-05-22 11:20:57 +0530

    22052024

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author sabas
+ *
+ */
+module SampleProjectForGit {
+}
---
+++ src/org/sam/Helloworld.java
@@ -0,0 +1,9 @@
+package org.sam;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!!");
+	}
+
+}
