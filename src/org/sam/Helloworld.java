797a44e6abd00f6f42012985543fb8d927343ced
Sabasiva2916 <sabasiva2916@gmail.com>
2024-05-23 11:19:26 +0530

    test works

--- src/org/sam/Helloworld.java
+++ src/org/sam/Helloworld.java
@@ -1,9 +1,24 @@
 package org.sam;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Helloworld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!!!");
+		List<String> str = new ArrayList<>();
+		str.add("java");
+		str.add("selenium");
+		str.add("python");
+		str.add("apitesting");
+		str.add("mobiletesting");
+		str.add("cypress");
+
+		// iterating List
+		for (String s : str) {
+			System.out.println(s);
+		}
 	}
 
 }
