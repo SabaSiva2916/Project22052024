38c37b5c2b78238179abb70e9cdc0585a1a2a5be
Sabasiva2916 <sabasiva2916@gmail.com>
2024-05-23 11:12:49 +0530

    dev works

--- src/org/sam/Helloworld.java
+++ src/org/sam/Helloworld.java
@@ -4,6 +4,15 @@ public class Helloworld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!!!");
+		// Reverse of String
+		String str = "Hello World";
+		String output = "";
+
+		for (int i = str.length() - 1; i >= 0; i--) {
+			char c = str.charAt(i);
+			output = output + c;
+		}
+		System.out.println("Reverse of given String :" + output);
 	}
 
 }
